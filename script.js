e06dd5d799b5250e66dc6211ce2d40eed1be19fe
Taslim <taslimahmed313@gmil.com>
2022-09-14 22:27:34 +0600

    secend Converted to Integer

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${parseInt(timeTaken)}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
@@ -111,7 +111,7 @@ const start = () => {
 
     // finished timer
     if (count == 0) {
-      console.log(count);
+      // console.log(count);
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "none";
@@ -136,5 +136,7 @@ setInterval(() => {
   const timeSpent = (currentTime - startTime) / 1000;
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${
+    parseInt(startTime) ? parseInt(timeSpent) : 0
+  } seconds`;
 }, 1000);
